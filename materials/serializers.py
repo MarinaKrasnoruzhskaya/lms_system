4f151a7e9ffe35cd0e451620da6d3ed20788df08
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-03 22:18:00 +0300

    add CRUD from Lesson by using Generics

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    """ Класс-сериализатор для модели Курс"""
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class LessonSerializer(ModelSerializer):
+    """ Класс-сериализатор для модели Урок"""
+    class Meta:
+        model = Lesson
         fields = '__all__'
--- materials/views.py
+++ materials/views.py
@@ -1,9 +1,40 @@
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course
-from materials.serializers import CourseSerializer
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(ModelViewSet):
+    """ ViewSet-класс для модели Курс """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(CreateAPIView):
+    """ Класс-контроллер на основе базового класса дженерика для создания урока """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(ListAPIView):
+    """ Класс-контроллер на основе базового класса дженерика для отображения списка уроков """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    """ Класс-контроллер на основе базового класса дженерика для отображения одного урока """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    """ Класс-контроллер на основе базового класса дженерика для редактирования урока """
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    """ Класс-контроллер на основе базового класса дженерика для удаления урока """
+    queryset = Lesson.objects.all()
