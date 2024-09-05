50d13666cdc4e17d775c1e808c64067ddce1ad57
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-05 21:30:22 +0300

    add dinamic output field for the number of lessons

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,13 +1,20 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
-    """ Класс-сериализатор для модели Курс"""
+    """ Класс-сериализатор для модели Курс, добавлено динамическое поле"""
+    count_lesson = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_count_lesson(self, course):
+        """ Метод возвращает количество уроков для объекта"""
+        return course.lesson_set.count()
 
 
 class LessonSerializer(ModelSerializer):
