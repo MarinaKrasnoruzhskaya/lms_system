2cf60039a17d77dc76aeb7ce8c277d6c8edb4c74
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-06 23:37:39 +0300

    add django_filters

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'rest_framework',
+    'django_filters',
     'phonenumber_field',
     'users',
     'materials',
@@ -78,6 +79,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.generics import ListAPIView
 
 from users.models import Payments
@@ -8,3 +10,8 @@ class PaymentsListAPIView(ListAPIView):
     """ Класс-контроллер на основе базового класса дженерика для отображения списка платежей """
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ('payment_date',)
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+
