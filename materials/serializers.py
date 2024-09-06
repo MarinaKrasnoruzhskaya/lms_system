482d757d9db6ddaed17fbb387a2fcd419438172b
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-06 16:54:20 +0300

    add field output lessons

--- materials/serializers.py
+++ materials/serializers.py
@@ -7,6 +7,7 @@ from materials.models import Course, Lesson
 class CourseSerializer(ModelSerializer):
     """ Класс-сериализатор для модели Курс, добавлено динамическое поле"""
     count_lesson = SerializerMethodField()
+    lessons = SerializerMethodField()
 
     class Meta:
         model = Course
@@ -15,6 +16,10 @@ class CourseSerializer(ModelSerializer):
     def get_count_lesson(self, course):
         """ Метод возвращает количество уроков для объекта"""
         return course.lesson_set.count()
+
+    def get_lessons(self, course):
+        """ Метод возвращает список уроков для объекта course """
+        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
 
 
 class LessonSerializer(ModelSerializer):
