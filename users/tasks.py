b978f7a2907e2970e2bc9a64cb81764c90baf895
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-26 23:13:18 +0300

    update block users

--- users/tasks.py
+++ users/tasks.py
@@ -1,16 +1,16 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 
 from users.models import User
-from users.services import get_required_datetime
 
 
 @shared_task
 def execute_block_user():
     """ Блокирует пользователей, если пользователь не заходил более месяца """
 
-    required_datetime = get_required_datetime(hours=0, days=30)
+    required_datetime = datetime.now() - timedelta(days=39)
     for user in User.objects.filter(last_login__lt=required_datetime):
-        print(user.email)
         user.is_active = False
         user.save()
         print(f'User {user.email} blocked')
